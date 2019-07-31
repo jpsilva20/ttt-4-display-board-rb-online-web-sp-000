1386a5fe53b0ee8ce56cf4fb960d7a4299de1002
Jennifer Silva <jennifersilva20@gmail.com>
2019-07-31 02:04:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board
-  puts "boar  |   |   "
+  puts "#{board[0]}  |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
